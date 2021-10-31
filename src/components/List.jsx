91e23d65a3e0970adb99e9fea733ee413b1e4291
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-01 04:38:47 +0530

    new design added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -10,33 +10,26 @@ let List = ({ data, price, rate }) => {
             return b.price - a.price;
           }
         })
-              .map((i) => (
-            
-                  
-                  
-                  
-          <div className="grid">
-            <article className="card">
-              <div className="card__data">
+        .map((i) => (
+          <div>
+            <div class="card">
+              <div class="card__image">
                 <img
-                  src="http://cdn.cnn.com/cnnnext/dam/assets/140430115517-06-comfort-foods.jpg"
-                  alt=""
+                  src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                  alt="Salad"
                 />
-                <div className="card__info">
-                  <h2>{i.name}</h2>
-                  <h3>{i.rating}</h3>
-                  <div className="payement">
-                    {i.payment.cash && i.payment.online
-                      ? "cash/online"
-                      : i.payment.cash
-                      ? "cash"
-                      : "online"}
-                  </div>
-                </div>
-                <h3 className="card__price">{i.price}</h3>
-                <button className="card__add">like</button>
               </div>
-              {/* <div>{i.name}</div>
+              <div class="card__info">
+                <div class="car__info--title">
+                  <h3>{i.name}</h3>
+                  <p>rating - {i.rating}</p>
+                </div>
+                <div class="card__info--price">
+                  <p>price- {i.price}</p>
+                </div>
+              </div>
+            </div>
+            {/* <div>{i.name}</div>
               <div className="rating">rating : {i.rating}</div>
               <div>price : {i.price}</div>
               <div className="payement">
@@ -46,7 +39,6 @@ let List = ({ data, price, rate }) => {
                   ? "cash"
                   : "online"}
               </div> */}
-            </article>
             <br />
           </div>
         ))}
